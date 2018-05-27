e5752e9ae9c70b90e99c8d84a020749cce392ae0
Alex Almora <alxalmora@gmail.com>
2018-05-27 14:09:06 -0500

    CleanUp 2

--- src/test/java/mx/itam/Backend/TestUtils.java
+++ src/test/java/mx/itam/Backend/TestUtils.java
@@ -11,50 +11,43 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestUtils {
-    private final static Logger logger =
-            Logger.getLogger(TestUtils.class.getName());
     @Test
     public void testCurrencyFormatOne() {
         double decimalNumber = 3.1415926535;
         int decimals = 4;
-        //logger.log(Level.INFO, Utils.currencyFormat(decimalNumber, decimals));
-        assert(Utils.currencyFormat(decimalNumber, decimals).equals("$3.1416"));
+        assertEquals("$3.1416",Utils.currencyFormat(decimalNumber, decimals));
     }
 
     @Test
     public void testCurrencyFormatTwo() {
         double decimalNumber = 123456;
         int decimals = 5;
-        //logger.log(Level.INFO, Utils.currencyFormat(decimalNumber, decimals));
-        assert(Utils.currencyFormat(decimalNumber, decimals).equals("$123,456.00000"));
+        assertEquals("$123,456.00000",Utils.currencyFormat(decimalNumber, decimals));
     }
 
     @Test
     public void testCurrencyFormatThree() {
         double decimalNumber = 0.35;
         int decimals = 2;
-        //logger.log(Level.INFO, Utils.currencyFormat(decimalNumber, decimals));
-        assert(Utils.currencyFormat(decimalNumber, decimals).equals("$0.35"));
+        assertEquals("$0.35",Utils.currencyFormat(decimalNumber, decimals));
     }
 
     @Test
     public void getDateFromEpochOne() {
         long timeInMillis = 0;
-        //logger.log(Level.INFO, Utils.getDateFromEpoch(timeInMillis));
-        assert("31/12/1969".equals(Utils.getDateFromEpoch(timeInMillis)));
+        assertEquals("31/12/1969",Utils.getDateFromEpoch(timeInMillis));
     }
 
     @Test
     public void getDateFromEpochTwo() {
         long timeInMillis = 1527395165000L;
-        //logger.log(Level.INFO, Utils.getDateFromEpoch(timeInMillis));
-        assert("26/05/2018".equals(Utils.getDateFromEpoch(timeInMillis)));
+        assertEquals("26/05/2018",Utils.getDateFromEpoch(timeInMillis));
     }
 
         @Test
     public void getDateFromEpochThree() {
         long timeInMillis = 829996196000L;
-        assert("20/04/1996".equals(Utils.getDateFromEpoch(timeInMillis)));
+        assertEquals("20/04/1996",Utils.getDateFromEpoch(timeInMillis));
     }
 
 
